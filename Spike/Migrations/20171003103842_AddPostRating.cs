50446a7109070c08d83d958cd4cfcc696764cfcd
Phil Richards <phil@plottwist.io>
2017-10-03 11:41:11 +0100

    Added rating to post

---
+++ Spike/Migrations/20171003103842_AddPostRating.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Spike.Migrations
+{
+    public partial class AddPostRating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Rating",
+                table: "Posts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Posts");
+        }
+    }
+}
